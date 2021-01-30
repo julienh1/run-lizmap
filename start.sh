cf020365486bd2987864a4dfb600b33cf4d82fb9
julienh1 <58294367+julienh1@users.noreply.github.com>
2021-01-30 22:22:15 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 apt-get install gdal-bin
 ogr2ogr -f "PostgreSQL" PG:"host=192.168.1.0 user=postgres password=postgres dbname=lizmap" departement17.geojson
-mkdir sig/qgis/geopoppy_pc
-cp run-lizmap/Poc* sig/qgis/geopoppy_pc
+mkdir ../sig/qgis/geopoppy_pc
+cp run-lizmap/Poc* ../sig/qgis/geopoppy_pc
